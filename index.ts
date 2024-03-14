7b3b0bc69736f501b59e89b8c6ed418e090f7fe6
Anis <aniskadhem09@gmail.com>
2024-03-14 15:08:01 +0100

    Class Accessors

--- index.ts
+++ index.ts
@@ -583,7 +583,7 @@ console.log(userOne.sayMsg());
 
   - TypeScript Is A Layer On Top Of JavaScript
   - It Should Remove All Annotations And Although Access Modifiers "Private For Example"
-*/
+
 
 class User {
   msg: () => string;
@@ -603,5 +603,49 @@ let userOne = new User("Elzero", 6000, "Cairo");
 // console.log(userOne.salary);
 console.log(userOne.msg());
 console.log(userOne.sayMsg());
+*/
 
-  /*_____________________End  Class Access Modifiers And Parameters Properties ____________________*/
+/*_____________________End  Class Access Modifiers And Parameters Properties ____________________*/
+
+
+/*_____________________20  Class Accessors ____________________*/
+
+/*
+  Class
+  - Get And Set Accessors
+*/
+
+class User {
+  public get username(): string {
+    return this._username;
+  }
+  public set username(value: string) {
+    this._username = value;
+  }
+  msg: () => string;
+  constructor(private _username: string, public salary: number, public readonly address: string) {
+    this.msg = function () {
+      return `Hello ${this._username} Your Salary Is ${this.salary}`;
+    }
+  }
+  sayMsg() {
+    return `Hello ${this._username} Your Salary Is ${this.salary}`;
+  }
+  // get username() : string {
+  //   return this._username;
+  // }
+  // set username(value: string) {
+  //   this._username = value;
+  // }
+}
+
+let userOne = new User("Elzero", 6000, "Cairo");
+
+console.log(userOne.username);
+userOne.username = "Ahmed";
+console.log(userOne.username);
+console.log(userOne.salary);
+console.log(userOne.msg());
+console.log(userOne.sayMsg());
+
+/*_____________________End  Class Accessors ____________________*/
